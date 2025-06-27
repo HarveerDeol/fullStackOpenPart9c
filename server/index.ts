2ff9b5691b767702c578400fe1cdd2588efb4961
Harveer Deol <harveerdeol53@gmail.com>
2025-06-27 11:42:08 -0600

    exercise 9.9

--- server/index.ts
+++ server/index.ts
@@ -1,8 +1,16 @@
 import express from 'express';
+const cors = require('cors');
 const app = express();
 app.use(express.json());
 
-const PORT = 3000;
+const PORT = 3001;
+
+const corsOptions = {
+  origin: 'http://localhost:5173', // Replace with your client's origin
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+};
+app.use(cors(corsOptions));
 
 app.get('/ping', (_req, res) => {
   console.log('someone pinged here');
